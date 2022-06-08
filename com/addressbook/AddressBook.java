c016c1052c9642f5ee2c987874d304a8361e963d
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-08 12:15:04 +0530

    Added : Ability to create contact

--- com/addressbook/AddressBook.java
+++ com/addressbook/AddressBook.java
@@ -1,7 +1,36 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Book[] books = new Book[2];
+
+    private static Book createContact() {
+        Book book = new Book();
+        System.out.println("Enter first name ");
+        book.setFirstName(scanner.nextLine());
+        System.out.println("Enter last name");
+        book.setLastName(scanner.nextLine());
+        System.out.println("Enter address");
+        book.setAddress(scanner.nextLine());
+        System.out.println("Enter city");
+        book.setCity(scanner.nextLine());
+        System.out.println("Enter state");
+        book.setState(scanner.nextLine());
+        System.out.println("Enter Email");
+        book.setEmail(scanner.nextLine());
+        System.out.println("Enter phone num");
+        book.setPhoneNum(Integer.parseInt(scanner.nextLine()));
+        System.out.println("Enter zip");
+        book.setZip(Integer.parseInt(scanner.nextLine()));
+
+        return book;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program ");
+        AddressBook.createContact();
     }
 }
